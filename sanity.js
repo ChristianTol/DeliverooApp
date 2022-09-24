d5b7176c09538dbca26aaa6f0e35f94b778f1148
ChristianTol <christian.tol1998@hotmail.com>
2022-09-24 11:59:13 +0200

    Sanity back-end installed and configured

---
+++ sanity.js
@@ -0,0 +1,15 @@
+const sanityClient = require('@sanity/client')
+const imageUrlBuilder = require('@sanity/image-url');
+
+const client = sanityClient({
+    projectId: '35pf9nbt',
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: '2021-10-21',
+})
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
+export default client;
