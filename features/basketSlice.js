db91bf9edea44bd23c0a4ab042c6aabb01301edc
ChristianTol <christian.tol1998@hotmail.com>
2022-09-25 16:31:18 +0200

    removeFromBasket function added

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -1,25 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   items: [],
-}
+};
 
 export const basketSlice = createSlice({
-  name: 'basket',
+  name: "basket",
   initialState,
   reducers: {
     addToBasket: (state, action) => {
-      state.items = [...state.items, action.payload]
+      state.items = [...state.items, action.payload];
     },
     removeFromBasket: (state, action) => {
-      state.value -= 1
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id);
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+            `Can't remove product (id: ${action.payload.id}) as it's not in the basket!`
+        );
+      }
+
+      state.items = newBasket;
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addToBasked, removeFromBasket } = basketSlice.actions
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
-export const selectBasketItems = state => state.basketSlice.items;
+export const selectBasketItems = (state) => state.basket.items;
 
-export default basketSlice.reducer
+export const selectBasketItemsWithId = (state, id) => 
+    state.basket.items.filter(item => item.id === id); 
+
+export default basketSlice.reducer;
